a33ef73f4f6bbcdf8ba332cfeb6cf4afe48ca6bc
fagner henrique flores <henriquefagner@hotmail.com>
2020-03-02 15:29:19 -0600

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Using-Comparison-Operators
+
 at he most basic level you can evaluate two variables using a comparison operator to return a true or false
+more info at the js file ok
---
+++ Using-Comparison-Operators/Using-Comparison-Operators.js
@@ -0,0 +1,79 @@
+var pass = 90; //Pass mark
+var score = 50; //score
+
+//checke if the user has passed
+var hasPssed = score >= pass;
+
+//Write  the message into the page
+var el = document.getElementById("condition2");
+el.textContent = "level passaed:" + hasPssed;
+
+var pass = 50; //Pass mark
+var score = 90; //score
+
+//checke if the user has passed
+var hasPssed = score >= pass;
+
+//Write  the message into the page
+var el = document.getElementById("condition3");
+el.textContent = "level passaed:" + hasPssed;
+
+var pass = 90; //Pass mark
+var score = 50; //score
+
+//checke if the user has passed
+var hasPssed = score >= pass;
+
+//Write  the message into the page
+var el = document.getElementById("condition4");
+el.textContent = "level passaed:" + hasPssed;
+
+var pass = 50; //Pass mark
+var score = 90; //score
+
+//checke if the user has passed
+var hasPssed = score >= pass;
+
+//Write  the message into the page
+var el = document.getElementById("condition5");
+el.textContent = "level passaed:" + hasPssed;
+
+var pass = 90; //Pass mark
+var score = 50; //score
+
+//checke if the user has passed
+var hasPssed = score >= pass;
+
+//Write  the message into the page
+var el = document.getElementById("condition6");
+el.textContent = "level passaed:" + hasPssed;
+
+var pass = 50; //Pass mark
+var score = 90; //score
+
+//checke if the user has passed
+var hasPssed = score >= pass;
+
+//Write  the message into the page
+var el = document.getElementById("condition7");
+el.textContent = "level passaed:" + hasPssed;
+
+var pass = 90; //Pass mark
+var score = 50; //score
+
+//checke if the user has passed
+var hasPssed = score >= pass;
+
+//Write  the message into the page
+var el = document.getElementById("condition8");
+el.textContent = "level passaed:" + hasPssed;
+
+var pass = 50; //Pass mark
+var score = 90; //score
+
+//checke if the user has passed
+var hasPssed = score >= pass;
+
+//Write  the message into the page
+var el = document.getElementById("condition9");
+el.textContent = "level passaed:" + hasPssed;
